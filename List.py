85bfd07e3414f2e91f4f8636aebf45eb1e3facff
Habib ERDEM <141074065+HabibErdem@users.noreply.github.com>
2023-08-09 22:55:33 +0300

    Add files via upload

---
+++ List.py
@@ -0,0 +1,59 @@
+# Örnekler
+
+#-1) liste oluşturma
+
+# liste_1=[1987,1071,1453,1989,2020,1953]
+#
+# liste_2=[55,23,567,"Hakan","mehmet"]
+#
+# liste_3=[89,87,45,["habib","ediz","demet"],"istanbul"]
+#
+# print(liste_1)
+# print(liste_2)
+# print(liste_3)
+
+#-2) Listeden indeks değeri çağırmak
+
+# araba_markalari=["mercedes","bmw","ford","ferrari","range rover"]
+# print(araba_markalari[0])
+# print(araba_markalari[1])
+# print(araba_markalari[2])
+# print(araba_markalari[3])
+# print(araba_markalari[4])
+
+# listelerde indeks sayıları 0 dan başlar. 0 listede görünen ilk indekstir.
+
+#-3) Mevcut listeye yeni eleman eklemek
+
+# araba_markalari=["mercedes","bmw","ford","ferrari","range rover"]
+# araba_markalari.append("fiat")
+# print(araba_markalari)
+
+# appaned fonksiyonu sayesinde var olan bir listeye yeni eleman ekleyebiliyoruz ancak eklenen değer listenin son indeksine eklenir.
+
+#-4) mevcut listede 4. sıraya eleman ekleme
+
+# araba_markalari=["mercedes","bmw","ford","ferrari","range rover"]
+# araba_markalari.insert(4,"mazda")
+# print(araba_markalari)
+
+# insert fonksiyonu sayesinde listede istediğimiz indeks sırasına yeni eleman ekleyebiliyoruz.
+
+#-5) listede eleman silmek
+
+# araba_markalari=["mercedes","bmw","ford","ferrari","range rover"]
+# araba_markalari.pop(3)
+# print(araba_markalari)
+
+# listeden eleman silmek için pop fonksiyonu kullanılabilir. Pop fonksiyonunda çıkan paranteze silinmesi istediğimiz indeks numarasını yazmamız gerekir.
+
+#-6) iki farklı listeyi birleştirme.
+
+# list_1=[2,4,3,]
+# list_2=["hasan","ahmet","mehmet"]
+# list_1.extend(list_2)
+# print(list_1)
+
+# iki farklı listeyi birleştirmek için extend fonksiyonu kullanılabilir.
+
+
